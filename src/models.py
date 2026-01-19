65ddb9e517c29af0dfcfd651b967f26e59a8c59a
David Alvarez Iglesias <daalvarezig@gmail.com>
2026-01-19 15:38:01 +0000

    Añadido Diagrama

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 
 db = SQLAlchemy()
 
@@ -10,6 +11,7 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    post = relationship("Post")
 
     def serialize(self):
         return {
@@ -17,3 +19,74 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    image_url: Mapped[str] = mapped_column(String(500), nullable=False)   
+
+    user = relationship("User") 
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "title": self.title,
+            "image_url": self.image_url,
+        }
+    
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
+    text: Mapped[str] = mapped_column(String(300), nullable=False)    
+
+    user = relationship("User")
+    post = relationship("Post")
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "post_id": self.post_id,
+            "image_url": self.text,
+        }
+    
+class Follower(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    followed_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    follower_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)  
+
+    followed = relationship("User", foreign_keys=[followed_id])
+    follower = relationship("User", foreign_keys=[follower_id])
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "followed_id": self.followed_id,
+            "follower_id": self.follower_id,
+        }
+    
+class Message(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    sender_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    receiver_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
+    content: Mapped[str] = mapped_column(String(500), nullable=False)
+
+    sender = relationship("User", foreign_keys=[sender_id])
+    receiver = relationship("User", foreign_keys=[receiver_id])
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "sender_id": self.sender_id,
+            "receiver_id": self.receiver_id,
+            "content": self.content,
+        } 
+    
